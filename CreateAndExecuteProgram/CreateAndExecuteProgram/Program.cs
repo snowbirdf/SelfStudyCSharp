939fba5669ee28c1d82b78fce345fe48491e372a
snowbird <snowbird.and.townie+github@gmail.com>
2019-10-31 17:37:00 +0900

    「プログラムの作成・実行」の解答

---
+++ CreateAndExecuteProgram/CreateAndExecuteProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateAndExecuteProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("皆様、はじめまして");
+        }
+    }
+}
